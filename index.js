ecb4599ac5f63875d10bb399d223e0047418276a
xarli <117117311+c4rli@users.noreply.github.com>
2023-02-21 22:51:52 +0000

    add questions and licenses

--- index.js
+++ index.js
@@ -2,15 +2,63 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { default: Choices } = require("inquirer/lib/objects/choices");
+
+const licenses = [
+  "CC0",
+  "MIT",
+  "GPL",
+  "Non Commercial License"
+]
 
 // array of questions for user
 const questions = [
-    "Project Title:" +
-    "Description:" +
-    "Installation Instructions:" +
-    "Usage Info" +
-    "Contribution Guidelines:" +
-    "Test Instructions:"
+  {
+    name: "projectTitle",
+    type: "input",
+    message: "Project Title:"
+  },
+  {
+    name: "projectDescription",
+    type: "input",
+    message: "Description:"
+  },
+  {
+    name: "projectInstallation",
+    type: "input",
+    message: "Installation Instructions:"
+  },
+  {
+    name: "projectUsage",
+    type: "input",
+    message: "Usage Info"
+  },
+  {
+    name: "projectContribution",
+    type: "input",
+    message: "Contribution Guidelines:"
+  },
+  {
+    name: "projectInstructions",
+    type: "input",
+    message: "Test Instructions:"
+  },
+  {
+    name: "projectLicense",
+    type: "list",
+    message: "Select License Type:",
+    choices: licenses
+  },
+  {
+    name: "githubUsername",
+    type: "input",
+    message: "Github Username:"
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Email:"
+  }
 ];
 
 // function to write README file
@@ -19,10 +67,24 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+  console.log(welcomeMessage());
+  askQuestions(questions);
+}
 
+function askQuestions(questions) {
+    inquirer
+      .prompt(questions)
+      .then((answer) => {
+        console.log(answer);
+      });
+}
+
+function welcomeMessage(){
+  return (`Welcome to c4rli's README generator
+  To use this tool you should enter the information prompted followed by the [enter] key
+  `)
 }
 
 // function call to initialize program
 init();
 
-console.log("Test");
